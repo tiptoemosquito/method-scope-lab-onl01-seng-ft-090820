339f6452340678bed2cf21a21b28f66365e7c928
Christina Howard <clhoward33@icloud.com>
2020-09-09 22:01:12 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,7 +1,7 @@
 def rescue_princess_peach
   status = "rescued"
-  puts catch_phrase "Hooray! Mario has rescued Princess Peach."
+  puts phrase (phrase "Hooray! Mario has rescued Princess Peach.")
 end
 
 rescue_princess_peach
-puts catch_phrase
+puts phrase
