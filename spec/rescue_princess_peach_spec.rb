14a081fd33ed0a1fbd84aaf8ca1c06376699e6b3
Christina Howard <clhoward33@icloud.com>
2020-09-09 21:38:52 +0000

    Automatically backed up by Learn

--- spec/rescue_princess_peach_spec.rb
+++ spec/rescue_princess_peach_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
-"#rescue_princess_peach"
-  it "outputs a message and sets a variable, status, that is not available outside of this method"
+describe "#rescue_princess_peach" do
+  it "outputs a message and sets a variable, status, that is not available outside of this method" do
     expect{rescue_princess_peach}.to output("Hooray! Mario has rescued Princess Peach.\n").to_stdout
     expect{puts status}.to raise_error(NameError)
 
