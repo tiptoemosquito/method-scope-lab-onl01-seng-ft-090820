6245f75ab8025eccc56d95fbc99d88b94a2ca79b
Christina Howard <clhoward33@icloud.com>
2020-09-09 22:03:40 +0000

    Automatically backed up by Learn

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,3 +1,9 @@
-def catch_phrase
-  puts phrase
+def rescue_princess_peach
+  status = "rescued"
+  puts "Hooray! Mario has rescued Princess Peach."
 end
+
+## Comment the below back in to see method scope in action!
+
+# rescue_princess_peach
+# puts status
