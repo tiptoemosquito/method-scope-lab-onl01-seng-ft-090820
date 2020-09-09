c8c8a3bc574f56a46737ea1f07e4271a097a0a51
Christina Howard <clhoward33@icloud.com>
2020-09-09 22:41:03 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -4,7 +4,5 @@ def rescue_princess_peach
   puts "Hooray! Mario has rescued Princess Peach."
 end
 
-## Comment the below back in to see method scope in action!
-
-# rescue_princess_peach
-# puta status
+def phrase
+end
